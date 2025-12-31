31a8ed2de1d01e5dd0937b9300ec773d83df6da3
Eugenio Rivera Ramos <eugeniorrivera@gmail.com>
2025-12-31 00:55:31 -0800

    Configure custom domain whoiseugen.io

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   output: "export",
-  basePath: "/euge-website",
-  assetPrefix: "/euge-website",
   images: {
     unoptimized: true,
   },
